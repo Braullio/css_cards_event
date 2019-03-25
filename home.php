cab469794f9264c29c10d60b8645d5ae3cfe6ab7
Braullio Vinnicius <braulliovg@gmail.com>
2019-03-25 17:35:33 -0300

    create(home.php) data page

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CSS_CARDS_EVENT</title>
+    <link rel="stylesheet" type= "text/css" href="css/style.css">
+  </head>
+  <body>
+    <div class="product">
+      <div class="imgbox">
+        <img src="img/image.png">
+      </div>
+      <div class="details">
+        <h2>brand Name<br><span>Men's Designer T-Shirt</span></h2>
+        <div class="price">$55.99</div>
+        <label>Sizes</label>
+        <ul>
+          <li>XS</li>
+          <li>S</li>
+          <li>M</li>
+          <li>XL</li>
+        </ul>
+        <label>Colors</label>
+        <ul class="colors">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <a href="#">add to cart</a>
+      </div>
+    </div>
+  </body>
+</html>
